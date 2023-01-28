a6ffb965e006ca86e2b358eb780a7071beebd1a2
bilawalbaig8 <53264313+bilawalbaig8@users.noreply.github.com>
2023-01-28 20:24:21 +0500

    newproject

---
+++ PutItemonSale.py
@@ -0,0 +1,65 @@
+
+gulahmedproducts = [
+    {
+     'SkuCode': 111,
+     'Name': "3PC Yarn Dyed Unstitched digital Printed Suit CLP-32046",
+     'isAvailable': "true",
+     'category': "unstiched",
+     'price': 4043,
+     'color': ["black", "red", "white"],
+     'totalStock': 2,
+     'isonSale': "false",
+     'reviews': [
+         {
+             'udId': 1,
+             'uName': "abc",
+             'ratings': 4.5,
+             'comments': "nice product",
+             'isApproved': "false"
+
+         },
+
+         {
+             'udId': 2,
+             'uName': "abc",
+             'ratings': 2.0,
+             'comments': "faulty product",
+             'isApproved': "false"
+         }
+     ]
+     },
+
+    {
+        'SkuCode': 111,
+        'Name': "3PC Yarn Dyed Unstitched digital Printed Suit CLP-32046",
+        'isAvailable': "true",
+        'category': "stiched",
+        'price': 4043,
+        'color': ["black", "red", "white"],
+        'totalStock': 2,
+        'isonSale': "true",
+    },
+
+    {
+        'SkuCode': 111,
+        'Name': "3PC Yarn Dyed Unstitched digital Printed Suit CLP-32046",
+        'isAvailable': 'true',
+        'category': "two-pice",
+        'price': 4043,
+        'color': ["black", "red", "white"],
+        'totalStock': 2,
+        'isonSale': "true"
+    },
+
+    {
+        'SkuCode': 111,
+        'Name': "3PC Yarn Dyed Unstitched digital Printed Suit CLP-32046",
+        'isAvailable': "true",
+        'price': 4043,
+        'color': ["black", "red", "white"],
+        'totalStock': 2,
+        'isonSale': "false",
+     }
+]
+
+print(gulahmedproducts)
