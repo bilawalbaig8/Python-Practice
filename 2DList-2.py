02b943a8ae1b996aaafc9be122492ea4b4337e3a
bilawalbaig8 <53264313+bilawalbaig8@users.noreply.github.com>
2023-02-22 18:08:12 +0500

    code

---
+++ 2DList-2.py
@@ -0,0 +1,19 @@
+'''
+
+For 2D operations perform these:
+1- Find maximum value column-wise
+2- Sort the matrix row-wise
+3- Sort the matrix column-wise
+Either ascending or descending
+
+'''
+
+m_list = [
+
+    [12,13,14],
+    [22,23,24],
+    [32,33,34]
+
+]
+
+
