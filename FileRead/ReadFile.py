b0ffced9691a73e97d80a6afa31fc70e2a56de75
bilawalbaig8 <53264313+bilawalbaig8@users.noreply.github.com>
2023-03-02 18:32:33 +0500

    Update

--- FileRead/ReadFile.py
+++ FileRead/ReadFile.py
@@ -18,15 +18,69 @@ def openFile(filename):
     data=file.read()
     return data
 
-# Founding the list of words
+def removebuzzwords(data):
+    buzzwords = ["as","if","the","was","in","off","of","this","a","at","had","and","also","is"]
+    impwords = []
+    for word in data.split(" "):
+        if word.lower() not in buzzwords:
+            impwords.append(word.lower())
+
+    return impwords
 
 file = openFile('Jinnah.txt')
+
+
+# buzzword remove
+
+without_buzzwords = removebuzzwords(file)
+# print(without_buzzwords)
+
+# Founding the list of words
+
+
+lowercased_document = file.lower().split()
 
 words_to_find = ["Pakistan", "Jinnah", "India","England", "Muslim" , "Hindu"]
 
 word_counts = {}
 
 for word in words_to_find:
-    word_counts[word] = file.lower().count(word.lower())
+    lowercased_word = word.lower()
+    if lowercased_word in lowercased_document:
+        word_counts[word] = lowercased_document.count(lowercased_word)
+    else:
+        word_counts[word] = 1
 
 print(word_counts)
+
+
+# 5 most recurring words with "Jinnah"
+
+words_to_find = "Jinnah"
+
+#collecting previous words
+word_collect = []
+recurring_word_count = {}
+
+previous_word = ''
+
+for word in without_buzzwords:
+    if word == words_to_find.lower():
+        word_collect.append(previous_word)
+    else:
+        previous_word = word
+
+# print(word_collect)
+
+# counting the recurring words
+
+for count in word_collect:
+    if count in recurring_word_count:
+        recurring_word_count[count] += 1
+    else:
+        recurring_word_count[count] = 1
+
+print(recurring_word_count)
+
+
+# counting Maximum recurring words
