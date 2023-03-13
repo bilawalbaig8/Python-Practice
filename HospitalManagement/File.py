79c6eec29f936274b8e016eced05238d697abe6c
bilawalbaig8 <53264313+bilawalbaig8@users.noreply.github.com>
2023-03-13 17:55:13 +0500

    update

--- HospitalManagement/File.py
+++ HospitalManagement/File.py
@@ -1,24 +1,35 @@
 def openFile(filename):
-    file=open(filename,'r')
-    data=file.read()
+    file = open(filename, 'r')
+    data = file.read()
     return data
+
+
 def appendHeader(filename):
     file = open(filename, 'a')
-    header="PatientId"+"\t"+"Name"+"\t"+"Contact"+"\t"+"Address"+"\t"+"BOD"+"\t"+"Gender"+"\t"+"AppDates"+"\t"+"Medicines"+"\t"+"Remarks"
+    header = "PatientId" + "\t" + "Name" + "\t" + "Contact" + "\t" + "Address" + "\t" + "BOD" + "\t" + "Gender" + "\t" + "AppDates" + "\t" + "Medicines" + "\t" + "Remarks"
     file.write(header)
 
-def appendRecord(filename,data):
-    file=open(filename,'a')
+
+def appendRecord(filename, data):
+    file = open(filename, 'a')
     file.write("\n")
     file.write(data)
     return data
 
+
 def readAllRecords(filename):
-    file=open(filename,'r')
-    rows=file.readlines()
+    file = open(filename, 'r')
+    rows = file.readlines()
     return rows
 
-def writenewfile(filename,data):
+
+def readHeader(filename):
+    file = open(filename, 'r')
+    rows = file.readline()
+    return rows
+
+
+def writenewfile(filename, data):
     file = open(filename, 'w')
     w = file.write(data)
     return w
--- HospitalManagement/HospitalManagementSystem.py
+++ HospitalManagement/HospitalManagementSystem.py
@@ -1,12 +1,32 @@
+"""
+
+Assignment 1:
+Console all the file data into a tabular format.
+
+Assignment 2:
+Add functionality to search a patient by his/her contact number
+
+Assignment 3:
+Convert the data from file into lists format
+Write the data from lists to file again
+
+Assignment 4:
+Add delete and update record functionality
+After Search by contact number
+
+"""
+
 from File import *
+
+
 def register():
-    pId=input("Enter patient id")
-    name=input("Enter patient name ")
-    dob=input("Enter date of birth for patient(DD-MM-YYYY) ")
-    address=input("Enter address of patient ")
-    gender=input("Enter gender M or F ")
-    contact=input("Enter contact number ")
-    info=[]
+    pId = input("Enter patient id")
+    name = input("Enter patient name ")
+    dob = input("Enter date of birth for patient(DD-MM-YYYY) ")
+    address = input("Enter address of patient ")
+    gender = input("Enter gender M or F ")
+    contact = input("Enter contact number ")
+    info = []
     info.append(pId)
     info.append(name)
     info.append(contact)
@@ -15,6 +35,8 @@ def register():
     info.append(gender)
 
     return info
+
+
 def welcome():
     print("*************************************")
     print()
@@ -22,46 +44,56 @@ def welcome():
     print()
     print("**************************************")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     welcome()
     file = "data.txt"
-    #appendHeader(file)
-    choice = int(input(" Enter 1: To Register New Patient\n Enter 2: To View all the Patients\n Enter 3: For Search\n Enter 4 to quite this menu : "))
+    # appendHeader(file)
+
+    choice = int(input(
+        " Enter 1: To Register New Patient\n "
+        "Enter 2: To View all the Patients\n "
+        "Enter 3: For Search\n "
+        "Enter 4: To Update the Record\n "
+        "Enter 5: For Quit "))
 
     if choice == 1:
         info = register()
         row = ''
         for r in range(len(info)):
-            row = row+info[r]+'\t'
-        appendRecord(file,row)
+            row = row + info[r] + '\t'
+        appendRecord(file, row)
 
     # Console all the file data into a tabular format.
 
-    elif choice==2:
+    elif choice == 2:
         records = readAllRecords(file)
         for i in range(len(records)):
             print(records[i])
 
-    # If block for searching
 
-    elif choice==3:
-        search = input("Enter 'Name or Contact Number' for search ")
+# If block for searching
+
+    elif choice == 3 or 4:
+        search = input("\nEnter 'Name or Contact Number' for search ")
         found = False
         records = readAllRecords(file)
         for record_list in records:
             if search in record_list:
-                print(record_list)
+                header = readHeader(file)
+                print("=====================================================================================")
+                print(header)
+                print(record_list, "\n \n")
                 found = True
                 break
 
         if not found:
-                print("Result not found")
+            print("\nResult not found")
 
-    elif choice == 4:
+    elif choice == 5:
         print("Quit")
 
-# Convert Data into List
-file = "data.txt"
-record = readAllRecords(file)
-writenewfile("RecordList.txt", str(record))
-
+# # Convert Data into List
+# file = "data.txt"
+# record = readAllRecords(file)
+# writenewfile("RecordList.txt", str(record))
