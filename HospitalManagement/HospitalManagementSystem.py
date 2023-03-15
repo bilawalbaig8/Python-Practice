56f487a848e7e83b145e5f16cc964c816aeaac78
bilawalbaig8 <53264313+bilawalbaig8@users.noreply.github.com>
2023-03-15 16:43:26 +0500

    update

--- HospitalManagement/HospitalManagementSystem.py
+++ HospitalManagement/HospitalManagementSystem.py
@@ -51,49 +51,55 @@ if __name__ == "__main__":
     # appendHeader(file)
     choice = None
 
-    while
+    while choice != 5:
 
-    choice = int(input(
-        " Enter 1: To Register New Patient\n "
-        "Enter 2: To View all the Patients\n "
-        "Enter 3: For Search\n "
-        "Enter 4: To Update the Record\n "
-        "Enter 5: For Quit "))
+        choice = int(input(
+            " Enter 1: To Register New Patient\n "
+            "Enter 2: To View all the Patients\n "
+            "Enter 3: For Search\n "
+            "Enter 4: To Update the Record\n "
+            "Enter 5: For Quit "))
 
-    if choice == 1:
-        info = register()
-        row = ''
-        for r in range(len(info)):
-            row = row + info[r] + '\t'
-        appendRecord(file, row)
+        if choice == 1:
+            info = register()
+            row = ''
+            for r in range(len(info)):
+                row = row + info[r] + '\t'
+            appendRecord(file, row)
 
-    # Console all the file data into a tabular format.
+        # Console all the file data into a tabular format.
 
-    elif choice == 2:
-        records = readAllRecords(file)
-        for i in range(len(records)):
-            print(records[i])
+        elif choice == 2:
+            records = readAllRecords(file)
+            for i in range(len(records)):
+                print(records[i])
 
-    # If block for searching
+        # If block for searching
 
-    elif choice == 3 or 4:
-        search = input("\nEnter 'Name or Contact Number' for search ")
-        found = False
-        records = readAllRecords(file)
-        for record_list in records:
-            if search in record_list:
-                header = readHeader(file)
-                print("=====================================================================================")
-                print(header)
-                print(record_list, "\n \n")
-                found = True
-                break
+        elif choice == 3 or 4:
+            search = input("\nEnter 'Name or Contact Number' for search ")
+            found = False
+            records = readAllRecords(file)
+            for record_list in records:
+                if search in record_list:
+                    header = readHeader(file)
+                    print("=====================================================================================")
+                    print(header)
+                    print(record_list, "\n \n")
+                    found = True
+                    break
 
-        if not found:
-            print("\nResult not found")
+            if not found:
+                print("\nResult not found")
 
-    elif choice == 5:
-        print("Quit")
+        elif choice == 5:
+            print("Quit")
+
+        subMenu = input("\nDO you want to continue? (Y/N)")
+
+        if subMenu != 'Y' and subMenu != 'y':
+            print("\nYou are quit")
+            break
 
 
 
